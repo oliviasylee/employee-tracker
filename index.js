449d3841ebacea8a87393a1a879d55e2b09d85aa
olivialee <oliviasylee@gmail.com>
2023-01-29 23:27:22 -0500

    Added addRole and installed figlet package

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2')
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const { default: Choices } = require('inquirer/lib/objects/choices');
+const figlet = require('figlet');
 
 require('dotenv').config();
 
@@ -14,11 +15,22 @@ const db = mysql.createConnection({
     port: 3306
 });
 
+
+
+figlet("Welcome !\n Employee Tracker", function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
 db.connect(function (err) {
     if(err) throw err;
     console.log('Welcome to the Employee Tracker!')
     menu()
-})
+});
 
 const menu = () => {
         return inquirer
@@ -31,17 +43,24 @@ const menu = () => {
                          choices: ['View All Employee', 'Add Employee', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
                      }
 ]).then((data) => {
-    if(data.startQuestions === 'Add Department'){
-        addDept();
-    } else if (data.startQuestions === 'View All Employee') {
+    if(data.startQuestions === 'View All Employee') {
         viewAllEmployee();
+    } else if (data.startQuestions === 'Add Employee'){
+        addEmployee();
     } else if (data.startQuestions === 'View All Roles') {
         viewAllRoles();
+    } else if (data.startQuestions === 'Add Role') {
+        addRole();
     } else if (data.startQuestions === 'View All Departments') {
         viewAllDepts();
-    }
-})
-};
+    } else if (data.startQuestions === 'Add Department'){
+        addDept();
+    } else if (data.startQuestions === 'Quit'){
+        console.log('The application is now closed. Goodbye!')
+        // or db.end();
+        process.exit();
+    }} 
+)};
 
 // Add Department
 const addDept = () => {
@@ -56,8 +75,8 @@ const addDept = () => {
          // deptData { department: "service"}
         ]).then((deptData) => {
             const query = "INSERT INTO department(department_name) VALUES(?)"
-            //to execute the SQL query and insert the department name into the database.
-            // first parameter -> sql query
+            // to execute the SQL query and insert the department name into the database.
+            // first parameter -> SQL query
             // seconde parameter -> object that contains the department name
             // third parameter -> callback function
             db.query(query, deptData.department, (err, result) => {
@@ -70,12 +89,92 @@ const addDept = () => {
             })
         };
 
+const addRole = () => {
+    // Use a promise to execute a query to retrieve the list of departments
+    db.promise().query("SELECT id, department_name FROM department")
+        .then(([rows]) => {
+            // Use the results to populate the choices in the inquirer prompt
+            const departmentChoices = rows.map(row => ({
+                name: row.department_name,
+                value: row.id
+            }));
+
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: "What is the name of the role?"
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: "What is the salary of the role?"
+                },
+                {
+                    type: 'list',
+                    name: 'departmentId',
+                    message: 'Which department does the role belong to?',
+                    choices: departmentChoices
+                }
+            ]).then((roleData) => {
+                const query = "INSERT INTO role(title, salary, department_id) VALUES(?, ?, ?)"
+
+                db.query(query, [roleData.title, roleData.salary, roleData.departmentId], (err, result) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`Added ${roleData.title} to the database`)
+                    menu();
+                });
+            });
+        })
+        .catch(console.error);
+};
+
 
 // Add Role 
-// Add Employee
-// Update Employee Role
+// const addRole = () => {
+//     inquirer.
+//        prompt([
+//            {
+//                type: 'input',
+//                name: 'title',
+//                message: "What is the name of the role?"
+//            },
+//            {
+//                 type: 'input',
+//                 name: 'salary',
+//                 message: "What is the salary of the role?"
+//             },
+//             {
+//                 type: 'list',
+//                 name: 'departmentName',
+//                 message: 'Which department does the role belong to?',
+                // 새로 입력된 디파트먼트 테이블에서 모두 불러와야함. choices에 sql query가 들어갈 수 있나?
+                // function이 들어올 수도 있음? 허? 그럼 viewdept 펑션이 들어와서 
+                // Choices array or a function returning a choices array. If defined as a function, the first parameter will be the current inquirer session answers. Array values can be simple numbers, strings, or objects containing a name (to display in list), a value (to save in the answers hash), and a short (to display after selection) properties. The choices array can also contain a Separator.
+                // 아 여기서 프로미스가 나와야하는구나 viewrole과 함께 해야하는 것 같은데 왜냐면 viewrole의 sql query가 필요하거든
+                // 이 프로미스의 목적이 뭐니? 사용자의 입력을 받아서 새로운 역할을 추가하는 것. 하지만 이것을 하는 동안 view role보는 것임? 
+//                 choices: [ ]
+//             }
+     
+        // roleData { title: "customer service", salary: "80000", department: :"Service" }
+//        ]).then((roleData) => {
+//            const query = "INSERT INTO role(title, salary, department_id) VALUES(?, ?, ?)"
+           
+//            db.query(query, [roleData.role, roleData.salary, roleData.departmentName], (err, result) => {
+//                if(err) {
+//                    throw(err);
+//                        } 
+//                console.log(`Added ${roleData.role} to the database`)
+//                menu();
+//            }); 
+//            })
+//        };
 
-// const addAllEmployees = () => {
+
+// Add Employee
+// const addEmployee = () => {
 //     inquirer.prompt([
 //         {
 //             type: 'input',
@@ -89,6 +188,8 @@ const addDept = () => {
 //         })
 //     })
 // }
+
+// Update Employee Role
 
 const viewAllEmployee = () => {
     const query = "SELECT employee.id, employee.first_name, employee.last_name, title, department_name AS department, salary, CONCAT(mng.first_name, ' ', mng.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee mng ON employee.manager_id = mng.id";
